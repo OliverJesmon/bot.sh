00d627ed4c79804288e6d3002ed08dc258e29023
OliverJesmon <102456180+OliverJesmon@users.noreply.github.com>
2022-03-27 12:10:33 +0530

    bot.sh
    Source code of my repository

---
+++ bot.sh
@@ -0,0 +1,14 @@
+                      
+#!/bin/bash
+ 
+echo "Welcome"
+sleep 5
+ 
+#Read the user input
+echo "May, I know what is your good name? :"
+ 
+read first_name
+ 
+echo "Hello $first_name"
+ 
+echo
