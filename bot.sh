dde7b4c11be13051c3de4b693aa8a0e0091be37c
OliverJesmon <102456180+OliverJesmon@users.noreply.github.com>
2022-04-13 09:03:34 +0530

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -9,6 +9,6 @@ echo "May, I know what is your good name? :"
  
 read first_name
  
-echo "Hello $first_name"
+echo "Hello $first_name.You are currently logged as $(whoami) and your ip address details are as shown: $(ifconfig) "
  
 echo
